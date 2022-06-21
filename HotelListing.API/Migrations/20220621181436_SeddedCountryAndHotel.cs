1d8203b097f717883f210b0c2a9820eda0ea6349
HSouhaib <hassouhaib@gmail.com>
2022-06-21 19:19:23 +0100

    Added EFCore, Creating class models and Seeding the Db

---
+++ HotelListing.API/Migrations/20220621181436_SeddedCountryAndHotel.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Migrations
+{
+    public partial class SeddedCountryAndHotel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "Egypte", "EG" },
+                    { 2, "Brazil", "BZ" },
+                    { 3, "Morocco", "MR" },
+                    { 4, "Belgium", "BG" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Brussels", 4, "Brussels Resort and Spa", 4.2999999999999998 },
+                    { 2, "Meknes", 3, "Meknes Resort and Spa", 4.2999999999999998 },
+                    { 3, "Charm-Sheikh", 1, "Egypte Resort and Spa", 4.2999999999999998 },
+                    { 4, "Rio-De-Jenero", 2, "Brazil Resort and Spa", 4.2999999999999998 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
